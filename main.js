351abe5c67bfc01159b4268bacec22ec76045668
Maxwell Dubin <maxdhs@gmail.com>
2022-07-19 19:10:40 -0400

    commit 2

---
+++ main.js
@@ -0,0 +1,21 @@
+// with git we've saved our code with different versions locally and on the cloud and we've copied code that others have wrote
+// git collaboration, how do we work on a project together?
+
+// how can i see if i already have git going in a project folder?
+// git status
+
+// how do we start git?
+// git init
+
+// how do we make our first commit?
+// git add . ; git commit -m "something"
+
+// how do i see how many versions of my project i've saved?
+// git log --oneline
+
+// how save to cloud on github
+// create new repo on github
+// local git has no idea where our new repo on github is
+// git remote to make the link
+
+//
